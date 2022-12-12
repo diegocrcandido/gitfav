f20d36005491c600f0e76b4141ef31eb125eace9
Diego Cezar <diego.cezar.rc@gmail.com>
2022-12-12 14:38:14 -0300

    Github Favoritos

---
+++ js/Favorites.js
@@ -0,0 +1,129 @@
+import { GithubUser } from "./GithubUser.js"
+
+export class Favorites {
+  constructor(root){
+    this.root = document.querySelector(root)
+    this.load()    
+  }
+  
+  load(){
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []    
+  }
+
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username){
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists){
+        throw new Error ('Usuário já cadastrado')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error ('Usuário não encontrado!')
+      }
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error){
+      alert(error.message)
+    }    
+  }
+
+  delete(user){
+    const filteredEntries = this.entries
+    .filter(entry => entry.login !== user.login) 
+
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root){
+    super(root)
+    this.tbody = this.root.querySelector('table tbody')
+    this.update()   
+    this.onadd() 
+  }
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root. querySelector('.search input')      
+      
+      this.add(value)
+    }
+    //Procurar após apertar a tecla ENTER
+    window.document.onkeyup = event => {
+      if(event.key === "Enter"){
+        const { value } = this.root. querySelector('.search input')
+
+        this.add(value)
+      }
+    }
+  }
+  update(){
+    this.emptyDisplay()
+    this.removeAllTr()
+    
+    this.entries.forEach(user => {
+      const row = this.creatRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').src = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositorie').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+      row.querySelector('.remove').onclick = () => {
+        const isConfirm = confirm('Tem certeza que deseja apagar esta linha?')
+
+        if(isConfirm){
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+  creatRow(){
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `   
+    <td class="user">
+      <img src="https://github.com/meliga.png" alt="Foto do usuário">
+      <a href="https://github.com/meliga" target="_blank">
+        <p>Diedro Meliga</p>
+        <span>meliga</span>
+      </a>
+    </td>
+    <td class="repositorie">1</td>
+    <td class="followers">1</td>
+    <td>
+      <button class="remove">Remover</button>
+    </td> `    
+
+    return tr
+  }
+  removeAllTr(){  
+    this.tbody.querySelectorAll('tr')
+    .forEach((tr) => {
+      tr.remove()
+    })
+  }
+  emptyDisplay(){
+    if (this.entries.length === 0){
+      this.root.querySelector('.displayState').classList.remove('hide')
+    } else {
+      this.root.querySelector('.displayState').classList.add('hide')
+    }
+  }
+}
